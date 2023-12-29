b644fe58bbc4c3c477b8a69a173476d9d94a462e
safaroque7 <faroque.computer@gmail.com>
2023-12-29 22:44:38 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    <div class="container mx-auto">
+        <div class="flex">
+            <div class="w-1/2">
+                <h2> Form </h2>
+                <form action="#">
+                    <div class="border">
+                        <label for="website-name"></label>
+                        <input type="text" name="website-name" id="website-name" placeholder="Website's name" class="w-full p-2">
+                    </div>
+                </form>
+            </div>
+            <div class="w-1/2"></div>
+        </div>
+    </div>
+
+</body>
+
+</html>
